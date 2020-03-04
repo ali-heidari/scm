444bf8332f2da96fbe902084c1e9f5391c5b3d73
ali <ali-heidari@outlook.com>
2020-03-04 16:09:51 +0330

    set default bind settings

--- src/component/bind.py
+++ src/component/bind.py
@@ -8,9 +8,21 @@ class BIND(BaseComponent):
 
     def __init__(self):
         ''' Init BIND instance '''
-        
+
         super().__init__()
 
     def init(self):
         self.run_command("yum -y install bind bind-utils")
-    
+
+    def listen_any(self):
+        ''' Listen to any IP address '''
+
+        with open('/etc/named.conf', 'r') as file: 
+            content = file.read()
+            content = content.replace("listen-on port 53 { 127.0.0.1; };","listen-on port 53 { 127.0.0.1; any; };") 
+
+    def set_default(self):
+        ''' Sets the default settings '''
+
+        self.listen_any()
+        
