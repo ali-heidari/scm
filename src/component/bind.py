d797550930905733471b0f55f5c35f012e1f50e5
ali <ali-heidari@outlook.com>
2020-03-06 09:26:00 +0330

    Creating zone files added to bind module

--- src/component/bind.py
+++ src/component/bind.py
@@ -28,25 +28,83 @@ class BIND(BaseComponent):
         named_content["options"]["allow-query"]["any"] = "True"
         iscpy.WriteToFile(named_content, [], "/etc/named.conf")
 
+    def create_reversed_zone_content(self,  domain: str):
+        ''' Create a reversed zone content for zone file '''
+
+        return ''' @   IN  SOA     ns1.{domain}. root.{domain}. (
+                                                1001    ;Serial
+                                                3H      ;Refresh
+                                                15M     ;Retry
+                                                1W      ;Expire
+                                                1D      ;Minimum TTL
+                                                )
+
+;Name Server Information
+@ IN  NS      ns1.{domain}.
+
+;Reverse lookup for Name Server
+10        IN  PTR     ns1.{domain}.
+
+;PTR Record IP address to HostName
+100      IN  PTR     www.{domain}.
+150      IN  PTR     mail.{domain}. '''.format(domain=domain)
+
+    def create_zone_content(self,  domain: str, ip: str):
+        ''' Create a zone content for zone file '''
+
+        return ''' @   IN  SOA     ns1.{domain}. root.{domain}. (
+                                                1001    ;Serial
+                                                3H      ;Refresh
+                                                15M     ;Retry
+                                                1W      ;Expire
+                                                1D      ;Minimum TTL
+                                                )
+
+;Name Server Information
+@      IN  NS      ns1.{domain}.
+
+;IP address of Name Server
+ns1 IN  A       {ip}
+
+;Mail exchanger
+{domain}. IN  MX 10   mail.{domain}.
+
+;A - Record HostName To IP Address
+www     IN  A       {ip}
+mail    IN  A       {ip}
+
+;CNAME record
+ftp     IN CNAME        www.{domain}. '''.format(domain=domain, ip=ip)
+
+    def create_zone(self, name: str):
+        ''' Create a zone content for named.conf as dictionary to use with iscpy.AddZone '''
+
+        return {
+            "zone \"%s\" IN " % name: {
+                "type": "master",
+                "file": "/var/named/%s.db" % name,
+                "allow-update": "{ none; }"
+            }
+        }
+
     def add_domain(self, domain: str, ip: str):
-        ''' Add a domain to named.conf '''
+        ''' Add a domain to named.conf and create zone files '''
 
         # Add zone
         named_content = iscpy.ParseISCString(
-            open("/etc/named.conf", 'r').read())
-        iscpy.AddZone("zone \""+domain+"\" IN {" +
-                      "type master;" +
-                      "file \"/var/named/"+domain+".db\";" +
-                      "allow-update { none; };" +
-                      "}; ", named_content)
-        iscpy.WriteToFile(named_content, [], "/etc/named.conf")
+            open("/etc/named.rfc1912.zones", 'r').read())
+
+        named_content = iscpy.AddZone(
+            self.create_zone(domain), named_content)
         # Add reverse zone
-        reversed_ip = ".".join(reversed(ip.split("."))[1:])
-        named_content = iscpy.ParseISCString(
-            open("/etc/named.conf", 'r').read())
-        iscpy.AddZone("zone \""+reversed_ip+".in-addr.arpa\" IN {" +
-                      "type master;" +
-                      "file \"/var/named/"+".".join(ip.split(".")[1:])+".db\";" +
-                      "allow-update { none; };" +
-                      "};", named_content)
-        iscpy.WriteToFile(named_content, [], "/etc/named.conf")
+        reversed_ip = ".".join(list(reversed(ip.split(".")))[1:])
+        named_content = iscpy.AddZone(
+            self.create_zone(reversed_ip+".in-addr.arpa"), named_content)
+        iscpy.WriteToFile(named_content, [],
+                          "/etc/named.rfc1912.zones")
+        # Add zone file
+        with open("/var/named/"+domain+".db", 'w') as file:
+            file.write(self.create_zone_content(domain, ip))
+        # Add reverse zone file
+        with open("/var/named/"+reversed_ip+".in-addr.arpa"+".db", 'w') as file:
+            file.write(self.create_reversed_zone_content(domain))
